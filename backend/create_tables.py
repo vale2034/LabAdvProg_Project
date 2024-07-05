c4356098c1dac2f1f6b9b646fe27867852599c1d
vale2034 <botte.2034421@studenti.uniroma1.it>
2024-07-05 16:48:13 +0200

    removing unuseful

--- backend/create_tables.py
+++
@@ -1,9 +0,0 @@
-from config import engine, Base
-from app.models import Product, User, CartItem, Order
-
-try:
-    # Crea tutte le tabelle definite nei modelli
-    Base.metadata.create_all(bind=engine)
-    print("Tabelle create con successo.")
-except Exception as e:
-    print(f"Si è verificato un errore durante la creazione delle tabelle: {str(e)}")
